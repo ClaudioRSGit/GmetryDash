25955b40178be8a7c6719d8951ecfaf091c8a530
claudiosilvaa63@gmail.com <claudiosilvaa63@gmail.com>
2023-02-24 20:09:30 +0000

    added some colors

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const gameContainer = document.getElementById('game');
 const character = document.getElementById('character');
+const tempoDisplay = document.getElementById('tempo');
 
+var tempo=0;
+
+col();
 gameContainer.addEventListener('click', () => {
 	character.classList.add('animate');
 	setTimeout(() => {
 		character.classList.remove('animate');
 	}, 500);
 });
+
+function col(){
+		tempo++;
+		tempoDisplay.innerHTML = tempo;
+	window.requestAnimationFrame(col);
+}
